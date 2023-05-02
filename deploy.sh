6d52706d769486cb235c66ad648bd8f688ce5370
NSSSSVODF23 <max95202@gmail.com>
2023-05-02 18:00:59 +0300

    Инструкции для запуска

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /root/crm/microelCRMFront/
+git pull
+mkdir -p /var/www/10.128.227.39
+yes | cp -R /root/crm/microelCRMFront/dist/front/* /var/www/10.128.227.39
+yes | cp /root/crm/microelCRMFront/10.128.227.39 /etc/nginx/sites-available/
+ln /root/crm/microelCRMFront/10.128.227.39 /etc/nginx/sites-enabled/
+systemctl restart nginx
