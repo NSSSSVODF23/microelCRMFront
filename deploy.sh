f6988bec67803b39fbff695227c32e78f3f64c24
NSSSSVODF23 <max95202@gmail.com>
2023-05-03 10:32:15 +0300

    Поправил файл развертки

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git pull
 mkdir -p /var/www/10.128.227.39
 yes | cp -R /root/crm/microelCRMFront/dist/front/* /var/www/10.128.227.39
 yes | cp /root/crm/microelCRMFront/10.128.227.39 /etc/nginx/sites-available/
-ln /root/crm/microelCRMFront/10.128.227.39 /etc/nginx/sites-enabled/
+ln -sf /root/crm/microelCRMFront/10.128.227.39 /etc/nginx/sites-enabled/
 systemctl restart nginx
